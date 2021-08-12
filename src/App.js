87584754386d8011d765c48f9e1c6a23d7bd4862
vinayak <vinayakcy123@gmail.com>
2021-08-12 13:35:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import './App.css';
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Service from "./Service";
+import Navbar from "./Navbar";
+import { Switch, Route, Redirect } from "react-router";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/service" component={Service} />
+      <Route exact path="/contact" component={Contact} />
+      <Redirect to="/" />
+    </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <section id="header" className="">
+                <div className="container-fluid nav-bg">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                                <h1>Grow Your business with <strong className="brand-name">Vinayak</strong></h1>
+                                <h2 className="my-3">We are the team of talented developer making websites</h2>
+                                <div className="mt-3">
+                                    <a href="" className="btn-get-started">Get Started</a>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                <img src="" className="img-fluid animated" alt="home-img" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+        </>
+    )
+}
+
+export default Home
